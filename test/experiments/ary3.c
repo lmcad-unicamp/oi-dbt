01ccf55ec7e808bbbcad33cb06e990c0ea8ae17d
vandersonmr <vandersonmr2@gmail.com>
2017-07-26 11:06:04 -0300

    llvm-test benchmarks

---
+++ test/experiments/ary3.c
@@ -0,0 +1,29 @@
+/* -*- mode: c -*-
+ * $Id: ary3.c 36673 2007-05-03 16:55:46Z laurov $
+ * http://www.bagley.org/~doug/shootout/
+ *
+ * this program is modified from:
+ *   http://cm.bell-labs.com/cm/cs/who/bwk/interps/pap.html
+ * Timing Trials, or, the Trials of Timing: Experiments with Scripting
+ * and User-Interface Languages by Brian W. Kernighan and
+ * Christopher J. Van Wyk.
+ *
+ * I added free() to deallocate memory.
+ */
+#define LENGTH 5000000
+int x[LENGTH];
+int y[LENGTH];
+
+int main(int argc, char *argv[]) {
+  int n = LENGTH;
+  int i, k;
+
+  for (i = 0; i < n; i++) 
+    x[i] = i + 1;
+
+  for (k=0; k<1000; k++) 
+    for (i = n-1; i >= 0; i--) 
+      y[i] += x[i];
+
+  return y[n-50];
+}
---
+++ test/experiments/dt.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+
+enum {
+  ITERATIONS = 231072,
+  size = 8048
+};
+
+static inline void
+double_array_divs_variable(double * __restrict dvec1,
+                           double * __restrict dvec2){
+  long i, j;
+  for(j = 0; j < ITERATIONS; j++)
+    for(i = 0; i < size; i++)
+      dvec1[i] /= dvec2[i];
+  
+// Can use loop interchange to turn this into:
+//  for(i = 0; i < size; i++) {
+//    double tmp1 = dvec1[i];
+//    double tmp2 = dvec2[i];
+//    for(j = 0; j < ITERATIONS; j++)
+//      tmp1 /= tmp2;
+//    dvec1[i] = tmp1;
+//  }
+
+// Can then hoist 1/tmp2 out of the loop if -ffast-math is enabled.
+}
+
+int main(int argc, char *argv[]) {
+  double	*dvec1, *dvec2;
+  long i;
+
+  dvec1 = malloc(size * sizeof(double));
+  dvec2 = malloc(size * sizeof(double));
+
+  printf( " %i iterations of each test. ", ITERATIONS );
+  printf( " inner loop / array size %i.\n", size );
+
+  // With better alias analysis of posix_memalign, we'd avoid reloading
+  // dvec1/dvec2 each time through the loop.
+  for( i = 0; i < size; i++ ) {
+          dvec1[i] = 1.0000001 * cosf((float)(size - i));
+          dvec2[i] = 1.0  + 0.0000000001 * sinf((float)i);
+  }
+
+  double_array_divs_variable( dvec1, dvec2 );
+  printf("%d\n", (int) 10000*dvec1[0]);
+  return 1000*dvec1[0];
+}
+
