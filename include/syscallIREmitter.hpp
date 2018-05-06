0c3c56b7b513c2b6baf3f5750001059f5e0c7fd5
Otavio Napoli <otavio.napoli@gmail.com>
2018-05-06 03:04:52 -0300

    new file:   syscallIREmitter.hpp

---
+++ include/syscallIREmitter.hpp
@@ -0,0 +1,36 @@
+#ifndef SYSCALLIREMITTER_HPP
+#define SYSCALLIREMITTER_HPP
+
+#include <OIDecoder.hpp>
+#include <syscall.hpp>
+#include <IREmitter.hpp>
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/CFG.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/InstIterator.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/IR/Type.h"
+#include "llvm/ExecutionEngine/MCJIT.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm-c/Core.h"
+#include "llvm-c/Disassembler.h"
+
+
+namespace dbt {
+  class SyscallIREmitter
+  {
+  private:
+    IREmitter& emitter;
+    void generateSyscallGet(const int syscallNo);
+
+  public:
+    SyscallIREmitter(IREmitter& E) : emitter(E) {};
+    void generateSyscallIR(llvm::LLVMContext& TheContext, llvm::Function* Func, std::unique_ptr<llvm::IRBuilder<>>& Builder, const uint32_t GuestAddr);
+  };
+}
+
+#endif
